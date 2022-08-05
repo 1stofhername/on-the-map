54922d34f27087200a2f33d27fb65dfdbf03d425
Alicia <arrowsrp@yahoo.com>
2022-08-04 19:11:56 -0700

    Adds class methods for getting mappers and trackers

--- app/models/mapper.rb
+++ app/models/mapper.rb
@@ -2,4 +2,20 @@ class Mapper < ActiveRecord::Base
     has_many :mapper_trackers
     has_many :trackers, through: :mapper_trackers
     has_many :posts
+
+    def get_trackers
+        self.trackers
+    end
+
+    def tracker_count
+        self.trackers.length
+    end
+
+    def get_posts
+        self.posts
+    end
+
+    def post_count
+        self.posts.count
+    end
 end
--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,4 +1,8 @@
 class Tracker < ActiveRecord::Base
     has_many :mapper_trackers
-    has_many :mappers, through: :mapper:trackers
+    has_many :mappers, through: :mapper_trackers
+
+    def get_mappers
+        self.mappers
+    end
 end
