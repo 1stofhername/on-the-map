af6068d592a7b3142a2a09581eb1bc7f476a7a55
Alicia <arrowsrp@yahoo.com>
2022-08-04 17:10:33 -0700

    Adds create_posts migration

---
+++ db/migrate/20220805000811_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :media
+      t.integer :location
+      t.string :details
+      t.integer :mapper_id
+      t.timestamps
+
+    end
+  end
+end
