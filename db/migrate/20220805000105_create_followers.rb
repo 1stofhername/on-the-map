6ae5b4c170ec8a4fe7b8be55e2f7f285779aac1f
Alicia <arrowsrp@yahoo.com>
2022-08-11 19:28:29 -0700

    Changes create_trackers migration to create_followers

--- db/migrate/20220805000105_create_followers.rb
+++ db/migrate/20220805000105_create_followers.rb
@@ -1,6 +1,6 @@
 class CreateTrackers < ActiveRecord::Migration[6.1]
   def change
-    create_table :trackers do |t|
+    create_table :followers do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
