5b967fd7b1d43e1a5353182e467d23318c2c9075
Alicia <arrowsrp@yahoo.com>
2022-08-11 19:20:04 -0700

    Alters schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,40 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2022_08_10_220111) do
 
-  create_table "mapper_trackers", force: :cascade do |t|
-    t.integer "tracker_id"
-    t.integer "mapper_id"
-    t.datetime "created_at"
-  end
-
-  create_table "mappers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "headline"
-    t.string "category"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "media"
-    t.integer "location"
-    t.string "details"
-    t.integer "mapper_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "trackers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
-  end
-
+  
 end
