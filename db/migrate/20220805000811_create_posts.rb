5f45271e8b21850f9818c03b1b5a754c10c89ca0
Alicia <arrowsrp@yahoo.com>
2022-08-24 20:49:14 -0700

    Changes posts table migration and adds get route for users

--- db/migrate/20220805000811_create_posts.rb
+++ db/migrate/20220805000811_create_posts.rb
@@ -1,8 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :media_link
+      t.string :image_file
       t.integer :location
       t.string :details
       t.integer :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -12,35 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2022_08_24_022235) do
 
-  create_table "followers", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "follower_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "media_link"
-    t.integer "location"
-    t.string "details"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "headline"
-    t.string "category"
-    t.string "password"
-    t.string "profile_img_link"
-    t.integer "follower_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "avatar"
-  end
-
+  
 end
